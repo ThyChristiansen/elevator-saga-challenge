cc3daac674581f1ce8a8f91bbc7cbc43ee025abe
Thy Christiansen <thyvu0411@gmail.com>
2021-01-28 15:15:05 -0600

    clean up code

--- #4-challenge.js
+++ #4-challenge.js
@@ -1,33 +1,20 @@
 {
     init: function(elevators, floors) {
-       
             elevators.map((elevator)=>{
                 elevator.on("idle", function() {
                     floors.map((floor)=>{
-
-
                         if(elevator.getPressedFloors().length > 0) {
                             elevator.goToFloor(elevator.getPressedFloors()[0]);
-                            floor.on('up_button_pressed down_button_pressed', function () {
-                                console.log(elevator.getPressedFloors()[0], floor.floorNum());
-                            });
                         }else{
                             if(elevator.loadFactor() === 0) {
-                                // Maybe use this elevator, since it's not full yet?
                                 elevator.goToFloor(0)
                             }else{
                                 elevator.goToFloor(floor)
-
                             }
-
                         }
                     })
-
                 });
-            
             })
-            
-
     },
         update: function(dt, elevators, floors) {
         }
