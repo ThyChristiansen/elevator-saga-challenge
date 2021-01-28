b60e8abdb84f6ddf317700a3d395a2da7d7ced4d
Thy Christiansen <thyvu0411@gmail.com>
2021-01-28 13:13:10 -0600

    clean up code

--- #2-challenge.js
+++ #2-challenge.js
@@ -4,7 +4,6 @@
         elevator.on("idle", function() {
             for (let i = 0 ; i < floors.length; i ++){
                 if(elevator.getPressedFloors().length > 0) {
-                    console.log("floor pressed ", this.getPressedFloors())
                     elevator.goToFloor(elevator.getPressedFloors()[0]);
                 }else{
                     elevator.goToFloor(i)
