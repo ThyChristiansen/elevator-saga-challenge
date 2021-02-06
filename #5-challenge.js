ec5b8e09369d93cf8580e602d4107959e3362556
Thy Christiansen <thyvu0411@gmail.com>
2021-02-05 19:12:39 -0600

    update code

--- #5-challenge.js
+++ #5-challenge.js
@@ -1,52 +1,67 @@
-{
-    init: function(elevators, floors) {
-        let pressFromFloor=[];
-        let passingFloor=[]
-        let matchingPassAndPressFloor= []
-        let upOrDown;
 
-        let createWaitList = (floor, elevator, ) =>{
+{
+    pressFromFloor:[],
+        passingFloor:[],   
+        matchingPassAndPressFloor:[],
+        upOrDown: "",
+        combineAllList:[],
+        waitList:[],
+        init: function(elevators, floors) {
+        let pressFromFloor=this.pressFromFloor;
+        let passingFloor=this.passingFloor;
+        let matchingPassAndPressFloor=this.matchingPassAndPressFloor;
+        let upOrDown= this.upOrDown;
+        let waitList = this.waitList;
+        let combineAllList= this.combineAllList;
+        let createPressFromFloorList = (floor) =>{
             floor.on('up_button_pressed down_button_pressed', function () {
                 pressFromFloor.push(floor.floorNum());
-                elevator.on("passing_floor", function(floorNum, direction) {
-                    passingFloor.push(floorNum)
-                    upOrDown = direction
-                    //find the matching passing floor and the floor in waitlist
-                    matchingPassAndPressFloor = passingFloor.filter(element => pressFromFloor.includes(element));
-                });
+            });
+        }
+        let createPassingFloorList= (elevator) =>{
+            elevator.on("passing_floor", function(floorNum, direction) {
+                passingFloor.push(floorNum);
+                upOrDown = direction;
+                //find the matching passing floor and the floor in waitlist
+                matchingPassAndPressFloor = passingFloor.filter(element => pressFromFloor.includes(element));
             });
         }
 
-
+        floors.forEach((floor)=>{
+            createPressFromFloorList(floor);
+        })
         elevators.forEach((elevator)=>{
+            createPassingFloorList(elevator);
+
+
+
             elevator.on("idle", function() {
                 if(elevator.loadFactor() > 0) {  
                     console.log("ele is NOT empty",elevator.loadFactor(), "pressFromFloor.length",pressFromFloor.length)
                     let goToList = [... new Set(elevator.getPressedFloors())];
                     if(elevator.loadFactor() < 1 && pressFromFloor) {
-                        floors.forEach((floor)=>{
-                            createWaitList(floor, elevator)
-                        })
-                        let combineAllList = matchingPassAndPressFloor.concat(pressFromFloor, goToList)
+
+                        combineAllList = matchingPassAndPressFloor.concat( goToList)
 
                         //delete duplicate
-                        let  waitList= [... new Set(combineAllList)]
+                        waitList = [... new Set(combineAllList)]
                         console.log("pressFromFloor", [... new Set(pressFromFloor)])
-                        console.log("passingFloor", [... new Set(passingFloor)])
-                        console.log("matchingPassAndPressFloor", [... new Set(passingFloor.filter(element => pressFromFloor.includes(element)))])
-                        console.log("goToList",goToList)
-                        console.log("waitList",waitList)
+                        //console.log("passingFloor", [... new Set(passingFloor)])
+                        //console.log("matchingPassAndPressFloor", [... new Set(passingFloor.filter(element => pressFromFloor.includes(element)))])
+                        //console.log("goToList",goToList)
+                        //console.log("waitList",waitList)
+                        //console.log("upOrDown",upOrDown)
 
-                        if(upOrDown === "up" ){
-                            console.log(upOrDown,waitList.sort((a, b) =>  b - a))
-                            goToList.sort((a, b) =>  b - a).forEach(x => elevator.goToFloor(x));
+                        if( upOrDown === "up"){
+                            console.log(upOrDown,waitList.sort((a, b) => a-b))
+                            waitList.sort((a, b) =>  a - b ).forEach(x => elevator.goToFloor(x));
                         }
                         else if( upOrDown === "down"){
-                            console.log(upOrDown, waitList.sort((a, b) => a - b))
-                            goToList.sort((a, b) => a - b).forEach(x => elevator.goToFloor(x));
+                            console.log(upOrDown, waitList.sort((a, b) => b - a))
+                            waitList.sort((a, b) => b-a).forEach(x => elevator.goToFloor(x));
                         }else{
                             console.log("not go up or down")
-                            waitList.forEach(x => elevator.goToFloor(x))
+                            waitList.sort((a, b) => a - b).forEach(x => elevator.goToFloor(x))
                         }
                     }
                     console.log("-------------------------")
@@ -56,15 +71,21 @@
                 else{
                     console.log("ele empty",elevator.loadFactor(), "pressFromFloor.length",pressFromFloor.length)
                     if(pressFromFloor.length > 0) {
-                        floors.forEach((floor)=>{
-                            createWaitList(floor, elevator)
-                        })
-                        let combineAllList = matchingPassAndPressFloor.concat(pressFromFloor)
+                        combineAllList = matchingPassAndPressFloor.concat(pressFromFloor)
                         //delete duplicate
-                        let waitList= [... new Set(combineAllList)]
-                        if(passingFloor.length > 0 && passingFloor.filter(element => pressFromFloor.includes(element))){
-                            console.log("passingFloor is true",passingFloor, waitList)
-                            waitList.map(x => elevator.goToFloor(x))
+                        waitList= [... new Set(combineAllList)]
+                        console.log("pressFromFloor", [... new Set(pressFromFloor)])
+                        //console.log("passingFloor", [... new Set(passingFloor)])
+                        //console.log("matchingPassAndPressFloor", [... new Set(passingFloor.filter(element => pressFromFloor.includes(element)))])
+                        //console.log("waitList",waitList)
+                        //console.log("upOrDown",upOrDown)
+
+                        if(matchingPassAndPressFloor.length > 0 && upOrDown === "up"){
+                            console.log("matchingPassAndPressFloor is true",matchingPassAndPressFloor,upOrDown, waitList)
+                            waitList.sort((a, b) =>  b - a).map(x => elevator.goToFloor(x))
+                        }else if(matchingPassAndPressFloor.length > 0 && upOrDown === "down"){
+                            console.log("matchingPassAndPressFloor is true",matchingPassAndPressFloor,upOrDown, waitList)
+                            waitList.sort((a, b) =>  a - b).map(x => elevator.goToFloor(x))
                         }else{
                             console.log("passingFloor is fault",passingFloor)
                             pressFromFloor.map(x => elevator.goToFloor(x))
@@ -77,7 +98,8 @@
                 }
             });
         })
-    },
+
+        },
         update: function(dt, elevators, floors) {
         }
 }
